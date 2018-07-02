e8a6c130621dc59631d6766e4719d3d4b07a1709
Brian Tracy <brian.tracy33@gmail.com>
2018-07-01 17:30:58 -0700

    Sun Jul  1 17:30:58 PDT 2018

--- publish.sh
+++ publish.sh
@@ -13,6 +13,7 @@ echo "</html>" >> index.html
 
 # Compress all images in the images folder into the compressed folder
 for img in images/*.{jpg,png}; do
+    [ -e "$img" ] || continue
     echo "Compressing $img"
     convert -resize 33% "$img" "compressed/`basename $img`"
 done
