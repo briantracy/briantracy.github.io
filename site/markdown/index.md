77d6cdf0b04d043cf29f5f0538606519c98ba664
Brian Tracy <brian.tracy33@gmail.com>
2019-11-24 03:37:49 -0500

    Automated commit: Sun Nov 24 03:37:49 EST 2019

--- site/markdown/index.md
+++ site/markdown/index.md
@@ -1,2 +1,3 @@
 
 This is the index page. `code block yomie`
+edit
--- site/render.sh
+++ site/render.sh
@@ -4,7 +4,7 @@
 gsed ':a;N;$!ba;s/\n//g;s/: /:/g;s/ {  /{/g;s/;  /;/g;s/;}/}/g' \
          ./templates/styles.css > ./templates/styles.min.css
 
-for file in ./markdown/*; do
+for file in $(git ls-files -m markdown); do
     printf "rendering $file\n"
 
     base=$(basename $file)
@@ -14,6 +14,7 @@ for file in ./markdown/*; do
            --template=./templates/template.html \
            --include-in-header=./templates/styles.min.css \
            --metadata title="Brian Tracy - $title" \
+           --metadata date="$(date)" \
            "$file" |
            gsed ':a;N;$!ba;s|>\s*<|><|g' \
            > "../${title}.html"
